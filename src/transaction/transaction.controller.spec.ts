e69ee7d6079cd7bdb5b97a18ddda7acc81a3441e
leonardo <leonardocesargoes@gmail.com>
2022-09-22 07:31:29 -0300

    Add: controller create method test

--- src/transaction/transaction.controller.spec.ts
+++ src/transaction/transaction.controller.spec.ts
@@ -1,18 +1,55 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { TransactionController } from './transaction.controller';
+import { TransactionService } from './transaction.service';
+import { PaymentOption } from './util/PayamentOption.enum';
 
 describe('TransactionController', () => {
-  let controller: TransactionController;
+  let transactionController: TransactionController;
+  let transactionService: TransactionService;
+  const transactionDto: CreateTransactionDto = {
+    amount: 2012,
+    paymentOption: PaymentOption.CREDIT_CARD,
+    cardNumber: '4350',
+    cardHolderName: 'Mario',
+    cardExpiration: '10/21',
+    cardVerificationCode: 423,
+    transactionDescription: 'descricao',
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TransactionController],
+
+      providers: [
+        {
+          provide: TransactionService,
+          useValue: {
+            create: jest
+              .fn()
+              .mockImplementation(
+                (createTransactionDto: CreateTransactionDto) => {
+                  Promise.resolve({ id: '2', ...createTransactionDto });
+                },
+              ),
+          },
+        },
+      ],
     }).compile();
 
-    controller = module.get<TransactionController>(TransactionController);
+    transactionController = module.get<TransactionController>(
+      TransactionController,
+    );
+    transactionService = module.get<TransactionService>(TransactionService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(transactionController).toBeDefined();
+  });
+  describe('createTransaction()', () => {
+    it('Should execute the transactionService.create() method ', () => {
+      transactionController.createTransaction(transactionDto);
+      expect(transactionService.create).toHaveBeenCalledWith(transactionDto);
+    });
   });
 });
--- src/transaction/transaction.module.ts
+++ src/transaction/transaction.module.ts
@@ -1,13 +1,23 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Payable } from 'src/payables/entities/Payable.entity';
+import { Repository } from 'typeorm';
 import { Transaction } from './entities/transaction.entity';
 import { TransactionController } from './transaction.controller';
 import { TransactionService } from './transaction.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Transaction, Payable])],
   controllers: [TransactionController],
-  providers: [TransactionService],
+  providers: [
+    TransactionService,
+    {
+      provide: Payable,
+      useClass: Repository,
+    },
+    {
+      provide: Transaction,
+      useClass: Repository,
+    },
+  ],
 })
 export class TransactionModule {}
